4664338ec74e789501e11d9539ee2d00e0e63eec
KishoreBabu <kishore.sai37@gmail.com>
2024-07-18 11:42:03 +0530

    add react-router and react-hot-toast

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,61 @@
-const App = () => {
-  return <h1>App</h1>;
-};
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export default App;
+import { Toaster } from 'react-hot-toast';
+
+// Define lazy-loaded components
+const Home = lazy(() => import('./pages/home.page'));
+const About = lazy(() => import('./pages/about.page'));
+const Profile = lazy(() => import('./pages/profile.page'));
+const Login = lazy(() => import('./pages/login.page'));
+const Register = lazy(() => import('./pages/register.page'));
+
+export default function App() {
+  // Return the BrowserRouter component with Routes and Route components
+  return (
+    <BrowserRouter>
+      {/* Load the sharing component header */}
+      <div>
+        <Toaster
+          position='top-right'
+          reverseOrder={false}
+        />
+      </div>
+
+      {/* Use the Suspense component to handle lazy loading */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Define the home route */}
+          <Route
+            path='/'
+            element={<Home />}
+          />
+
+          {/* Define the about route */}
+          <Route
+            path='/about'
+            element={<About />}
+          />
+
+          {/* Define the login route */}
+          <Route
+            path='/login'
+            element={<Login />}
+          />
+
+          {/* Define the register route */}
+          <Route
+            path='/register'
+            element={<Register />}
+          />
+
+          {/* Define the profile route */}
+          <Route
+            path='/profile'
+            element={<Profile />}
+          />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
---
+++ client/src/routes/index.js
@@ -0,0 +1,7 @@
+import HomePage from './home.page';
+import AboutPage from './about.page';
+import LoginPage from './login.page';
+import RegisterPage from './register.page';
+import ProfilePage from './profile.page';
+
+export { HomePage, AboutPage, LoginPage, RegisterPage, ProfilePage };
